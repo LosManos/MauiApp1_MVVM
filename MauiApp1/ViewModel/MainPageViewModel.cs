6a1003171bf0a2564433280d4e6abb45e8ba0481
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2023-05-01 20:18:15 +0200

    Converted

---
+++ MauiApp1/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiApp1.ViewModel;
+
+public partial class MainPageViewModel : ObservableObject
+{
+    public MainPageViewModel()
+    {
+        ButtonText = "Click Me";
+    }
+
+    [ObservableProperty]
+    public int myCount;
+
+    [ObservableProperty]
+    public string buttonText;
+
+    [RelayCommand]
+    public void IncreaseCount()
+    {
+        MyCount += 1;
+
+        if (MyCount == 1)
+            ButtonText = $"Clicked {MyCount} time";
+        else
+            ButtonText = $"Clicked {MyCount} times";
+    }
+}
